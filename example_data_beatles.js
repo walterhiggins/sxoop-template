ff7d8cd496f0b1fe19e979b46111299799732ff1
walterhiggins <walter.higgins@gmail.com>
2012-08-26 14:07:26 +0100

    example data

---
+++ example_data_beatles.js
@@ -0,0 +1,5 @@
+var beatles = [{name: "John Lennon", instrument: "Vocals/Guitar"},
+              {name: "Paul McCartney",  instrument: "Vocals/Bass"},
+              {name: "George Harrison", instrument: "Guitar"},
+              {name: "Ringo Starr", instrument: "Drums"}
+             ];
---
+++ example_data_stones.js
@@ -0,0 +1,5 @@
+var stones = [{name: "Mick Jagger", instrument: "Vocals"},
+              {name: "whatsisname",  instrument: "Vocals/Bass"},
+              {name: "Keith Richards", instrument: "Guitar"},
+              {name: "Charlie Watts", instrument: "Drums"}
+             ];
